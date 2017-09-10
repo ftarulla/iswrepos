7137a72fdbff05ec5c8ec9bb9b58d77bf0f156a2
Fran <ftarulla@gmail.com>
2017-09-10 15:30:59 -0300

    Filtrado de teams por ids pasados por parámetro

--- iswrepos.js
+++ iswrepos.js
@@ -16,10 +16,17 @@ cliOptions
     .version('0.1.0')
     .option('-t, --teams', 'shows teams info')
     .option('-s, --status', 'shows repositories status')
+    .option('-i, --ids <ids>', 'filter teams by ids', (val) => { return val.split(',')})
     .option('-c, --clone', 'clones repositories')
     .option('-f, --force', 'force the given action')
     .parse(process.argv);
 
+// filter teams by id?
+if(cliOptions.ids && cliOptions.ids.length > 0) {
+    teams = teams.filter(team => cliOptions.ids.includes(team.id));
+}
+
+//
 if(cliOptions.teams && !cliOptions.status) {
     commands.listTeams(teams);
 }
